a6566a91b58faf3a3935ff54aca061c329fcda85
Alexander Wieser <alexander.wieser@crystalbyte.de>
2013-09-22 20:59:16 +0200

    Added Composition

--- src/Crystalbyte.Paranoia/App.xaml.cs
+++ src/Crystalbyte.Paranoia/App.xaml.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Composition;
+using System.Composition.Hosting;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Crystalbyte.Paranoia.Contexts;
 
 namespace Crystalbyte.Paranoia {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application {
+    public partial class App {
+
+        [Import]
+        public static AppContext AppContext { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+
+            Compose();
+        }
+
+        private void Compose() {
+            var config = new ContainerConfiguration()
+                .WithAssembly(typeof (App).Assembly);
+
+            Composition = config.CreateContainer();
+            Composition.SatisfyImports(this);
+        }
+
+        public CompositionHost Composition { get; set; }
     }
 }
---
+++ src/Crystalbyte.Paranoia/Contexts/ImapAccountContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crystalbyte.Paranoia.Contexts {
+    public sealed class ImapAccountContext {
+        
+    }
+}
