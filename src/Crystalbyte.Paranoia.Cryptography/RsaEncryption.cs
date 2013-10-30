33fb29c70d54a74adf9262b4e8c6129c2abb2b5c
Sebastian Thobe <sebastianthobe@googlemail.com>
2013-10-30 23:56:56 +0100

    adding more rsa c library bindings

--- src/Crystalbyte.Paranoia.Cryptography/RsaEncryption.cs
+++ src/Crystalbyte.Paranoia.Cryptography/RsaEncryption.cs
@@ -30,17 +30,38 @@ namespace Crystalbyte.Paranoia.Cryptography {
         }
 
         private static class NativeMethods {
-            [DllImport(OpenSsl.Library, EntryPoint = "RAND_seed")]
-            public static extern void RandSeed(IntPtr buf, int num);
-
             [DllImport(OpenSsl.Library, EntryPoint = "RSA_new")]
             public static extern IntPtr RsaNew();
+
+            [DllImport(OpenSsl.Library, EntryPoint = "RSA_free")]
+            public static extern void RsaFree(IntPtr handle);
 
             [DllImport(OpenSsl.Library, EntryPoint = "RSA_generate_key_ex")]
             public static extern IntPtr RsaGenerateKeyEx(IntPtr rsa, int bits, IntPtr e, IntPtr cb);
 
-            [DllImport(OpenSsl.Library, EntryPoint = "RSA_free")]
-            public static extern void RsaFree(IntPtr handle);
+            [DllImport(OpenSsl.Library, EntryPoint = "RSA_public_encrypt")]
+            public static extern int RSA_public_encrypt(int flen, IntPtr from, IntPtr to, IntPtr rsa, int padding);
+
+            [DllImport(OpenSsl.Library, EntryPoint = "RSA_public_decrypt")]
+            public static extern int RSA_public_decrypt(int flen, IntPtr from, IntPtr to, IntPtr rsa, int padding);
+
+            [DllImport(OpenSsl.Library, EntryPoint = "RSA_private_encrypt")]
+            public static extern int RSA_private_encrypt(int flen, IntPtr from, IntPtr to, IntPtr rsa, int padding);
+
+            [DllImport(OpenSsl.Library, EntryPoint = "RSA_private_decrypt")]
+            public static extern int RSA_private_decrypt(int flen, IntPtr from, IntPtr to, IntPtr rsa, int padding);
+
+            [DllImport(OpenSsl.Library, EntryPoint = "RSA_sign")]
+            public static extern int RSA_sign(int type, IntPtr m, uint m_len, IntPtr sigret, IntPtr siglen, IntPtr rsa);
+
+            [DllImport(OpenSsl.Library, EntryPoint = "RSA_verify")]
+            public static extern int RSA_verify(int type, IntPtr m, uint m_len, IntPtr sigbuf, uint siglen, IntPtr rsa);
+
+            [DllImport(OpenSsl.Library, EntryPoint = "RSA_size")]
+            public static extern int RSA_size(IntPtr rsa);
+
+            [DllImport(OpenSsl.Library, EntryPoint = "RSA_check_key")]
+            public static extern int RSA_check_key(IntPtr rsa);
         }
 
         [StructLayout(LayoutKind.Sequential)]
