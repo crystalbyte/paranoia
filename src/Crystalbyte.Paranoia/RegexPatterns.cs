c4031a2f528560079cd5ab62930082cd35fc8937
Anubis89Berlin <MSchluch@hotmail.de>
2014-02-26 21:58:46 +0100

    fixed regex-pattern to accept uppercase

--- src/Crystalbyte.Paranoia/RegexPatterns.cs
+++ src/Crystalbyte.Paranoia/RegexPatterns.cs
@@ -1,6 +1,6 @@
 ﻿namespace Crystalbyte.Paranoia {
     internal static class RegexPatterns {
         public const string Email =
-            @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+            @"[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?";
     }
 }
