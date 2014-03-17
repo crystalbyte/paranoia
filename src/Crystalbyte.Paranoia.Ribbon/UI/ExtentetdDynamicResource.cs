0d407c2d4b421aed13611e91d24190e74fd84d1e
Anubis89Berlin <MSchluch@hotmail.de>
2014-03-17 22:25:47 +0100

    dynamic resource converter

---
+++ src/Crystalbyte.Paranoia.Ribbon/UI/ExtentetdDynamicResource.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace Crystalbyte.Paranoia.UI {
+    public class ExtentetdDynamicResource : DynamicResourceExtension {
+        public ExtentetdDynamicResource() {
+        }
+
+        public ExtentetdDynamicResource(object resourceKey)
+            : base(resourceKey) {
+        }
+
+        public System.Windows.Data.IValueConverter Converter { get; set; }
+        public object ConverterParameter { get; set; }
+
+        public override object ProvideValue(IServiceProvider provider) {
+            object value = base.ProvideValue(provider);
+            if (value != this && Converter != null) {
+                Type targetType = null;
+                var target = (IProvideValueTarget)provider.GetService(typeof(IProvideValueTarget));
+                if (target != null) {
+                    DependencyProperty targetDp = target.TargetProperty as DependencyProperty;
+                    if (targetDp != null) {
+                        targetType = targetDp.PropertyType;
+                    }
+                }
+                if (targetType != null)
+                    return Converter.Convert(value, targetType, ConverterParameter, CultureInfo.CurrentCulture);
+            }
+
+            return value;
+        }
+    }
+}
