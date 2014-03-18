5e4bde40775a939f28755e0778d9ec27ce35a7fc
crystalbyte <alexander.wieser@crystalbyte.de>
2014-03-18 11:38:45 +0100

    Fixed ribbon slide down animation
    Added clipping to parent element

--- src/Crystalbyte.Paranoia.Ribbon/UI/RibbonWindow.xaml.cs
+++ src/Crystalbyte.Paranoia.Ribbon/UI/RibbonWindow.xaml.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Threading.Tasks;
