287f486bbc684f29cb3975c51c329e4166ae7c07
Marco <m.gester@gmx.net>
2014-08-02 21:38:06 +0200

    Fixed NIL in Subject

--- src/Crystalbyte.Paranoia/UI/Converters/SubjectFormater.cs
+++ src/Crystalbyte.Paranoia/UI/Converters/SubjectFormater.cs
@@ -9,13 +9,25 @@ using System.Windows.Data;
 
 namespace Crystalbyte.Paranoia.UI.Converters {
     [ValueConversion(typeof (string), typeof (string))]
-    public sealed class IgnoreLinebreaksConverter : IValueConverter {
+    public sealed class SubjectFormater : IValueConverter {
         private const string Sub = " \u200B";
         private const string Pattern = "\n|\r";
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             var s = (string) value;
-            return string.IsNullOrEmpty(s) ? s : Regex.Replace(s, Pattern, Sub);
+
+            if (string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+            if (s.Equals("NIL"))
+            {
+                return s = Properties.Resources.NoSubject;
+            }
+           else
+            {
+                return Regex.Replace(s, Pattern, Sub);
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
