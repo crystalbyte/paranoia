de8215094291c7613afd29b2707f8ebd1502e729
Marco <m.gester@gmx.net>
2014-08-04 19:54:18 +0200

    Another WorkInProgress ReplyCommand

--- src/Crystalbyte.Paranoia/UI/Pages/ComposeMessagePage.xaml.cs
+++ src/Crystalbyte.Paranoia/UI/Pages/ComposeMessagePage.xaml.cs
@@ -3,14 +3,19 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Navigation;
+using System.Data.Entity;
+using System.Linq;
 
-namespace Crystalbyte.Paranoia.UI.Pages {
+namespace Crystalbyte.Paranoia.UI.Pages
+{
     /// <summary>
     /// Interaction logic for WriteMessagePage.xaml
     /// </summary>
-    public partial class ComposeMessagePage : INavigationAware {
+    public partial class ComposeMessagePage : INavigationAware
+    {
 
-        public ComposeMessagePage() {
+        public ComposeMessagePage()
+        {
             var context = new MailCompositionContext();
             context.Finished += OnShutdownRequested;
             context.DocumentTextRequested += OnDocumentTextRequested;
@@ -21,55 +26,56 @@ namespace Crystalbyte.Paranoia.UI.Pages {
             window.OverlayChanged += OnOverlayChanged;
         }
 
-        private static void OnShutdownRequested(object sender, EventArgs e) {
+        private static void OnShutdownRequested(object sender, EventArgs e)
+        {
             App.Context.CloseOverlay();
         }
 
-        private void OnDocumentTextRequested(object sender, DocumentTextRequestedEventArgs e) {
+        private void OnDocumentTextRequested(object sender, DocumentTextRequestedEventArgs e)
+        {
             e.Document = HtmlControl.GetDocument();
         }
 
-        private async void Reset() {
+        private async void Reset()
+        {
             var composition = (MailCompositionContext)DataContext;
             await composition.ResetAsync();
         }
 
-        private void OnOverlayChanged(object sender, EventArgs e) {
+        private void OnOverlayChanged(object sender, EventArgs e)
+        {
             var window = (MainWindow)Application.Current.MainWindow;
-            if (!window.IsOverlayVisible) {
+            if (!window.IsOverlayVisible)
+            {
                 RecipientsBox.Close();
             }
         }
 
-        public MailCompositionContext Composition {
+        public MailCompositionContext Composition
+        {
             get { return (MailCompositionContext)DataContext; }
         }
 
-        private async void OnRecipientsBoxItemsSourceRequested(object sender, ItemsSourceRequestedEventArgs e) {
+        private async void OnRecipientsBoxItemsSourceRequested(object sender, ItemsSourceRequestedEventArgs e)
+        {
             await Composition.QueryRecipientsAsync(e.Text);
         }
 
         #region Implementation of INavigationAware
 
-        public void OnNavigated(NavigationEventArgs e) {
+        public void OnNavigated(NavigationEventArgs e)
+        {
             Reset();
-
             if (!String.IsNullOrEmpty(e.Uri.OriginalString))
             {
-                 Regex ItemRegex = new Regex(@"[0-9]+", RegexOptions.Compiled);
-                foreach (Match ItemMatch in ItemRegex.Matches(e.Uri.OriginalString))
-                {
-                    Console.WriteLine(ItemMatch);
-                }
+                PrepareAsReply(e.Uri.OriginalString);
             }
-           
-
-           
         }
 
         #endregion
 
-        private void OnRecipientsBoxSelectionChanged(object sender, EventArgs e) {
+        private void OnRecipientsBoxSelectionChanged(object sender, EventArgs e)
+        {
             var addresses = RecipientsBox
                 .SelectedValues
                 .Select(x => x is MailContactContext
@@ -79,6 +85,21 @@ namespace Crystalbyte.Paranoia.UI.Pages {
             var context = (MailCompositionContext)DataContext;
             context.Recipients.Clear();
             context.Recipients.AddRange(addresses);
-        } 
+        }
+
+
+        private static async void PrepareAsReply(string s)
+        {
+            Data.MimeMessageModel[] message;
+            Regex ItemRegex = new Regex(@"[0-9]+", RegexOptions.Compiled);
+
+            using (var database = new Crystalbyte.Paranoia.Data.DatabaseContext())
+            {
+                Int64 temp = Int64.Parse(ItemRegex.Match(s).Value);
+                message = await database.MimeMessages
+                .Where(x => x.MessageId == temp)
+                .ToArrayAsync();
+            }
+        }
     }
 }
