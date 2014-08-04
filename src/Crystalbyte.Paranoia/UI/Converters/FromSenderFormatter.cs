235efc997f7f0da90d49e828059f773707da30b6
Marco <m.gester@gmx.net>
2014-08-04 17:51:42 +0200

    No Sender name now shows email address

---
+++ src/Crystalbyte.Paranoia/UI/Converters/FromSenderFormatter.cs
@@ -0,0 +1,33 @@
+﻿#region Using directives
+
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+
+#endregion
+
+namespace Crystalbyte.Paranoia.UI.Converters {
+    [ValueConversion(typeof (string), typeof (string))]
+    public sealed class FromSenderFormatter : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            var s = (MailMessageContext) value;
+
+            if (s.FromName.Equals("NIL") || string.IsNullOrEmpty(s.FromName))
+            {
+                return s.FromAddress;
+            }
+           else
+            {
+                return s.FromName;
+            }
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
