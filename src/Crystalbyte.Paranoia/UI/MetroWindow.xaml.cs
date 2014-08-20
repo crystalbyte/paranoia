abf1eaf3749c6d2280213d81e18e3d7331e34c48
crystalbyte <alexander.wieser@crystalbyte.de>
2014-08-21 00:00:42 +0200

    Added min-max window handling.

--- src/Crystalbyte.Paranoia/UI/MetroWindow.xaml.cs
+++ src/Crystalbyte.Paranoia/UI/MetroWindow.xaml.cs
@@ -24,8 +24,8 @@ namespace Crystalbyte.Paranoia.UI {
         #region Construction
 
         static MetroWindow() {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof (MetroWindow),
-                new FrameworkPropertyMetadata(typeof (MetroWindow)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MetroWindow),
+                new FrameworkPropertyMetadata(typeof(MetroWindow)));
         }
 
         public MetroWindow() {
@@ -60,17 +60,17 @@ namespace Crystalbyte.Paranoia.UI {
 
 
         public Thickness FramePadding {
-            get { return (Thickness) GetValue(FramePaddingProperty); }
+            get { return (Thickness)GetValue(FramePaddingProperty); }
             set { SetValue(FramePaddingProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for FramePadding.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty FramePaddingProperty =
-            DependencyProperty.Register("FramePadding", typeof (Thickness), typeof (MetroWindow),
+            DependencyProperty.Register("FramePadding", typeof(Thickness), typeof(MetroWindow),
                 new PropertyMetadata(new Thickness(0), OnFramePaddingChanged));
 
         private static void OnFramePaddingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
-            var window = (MetroWindow) d;
+            var window = (MetroWindow)d;
             window.InitFramePadding((Thickness)e.NewValue);
         }
 
@@ -79,50 +79,50 @@ namespace Crystalbyte.Paranoia.UI {
         }
 
         public bool IsNormalized {
-            get { return (bool) GetValue(IsNormalizedProperty); }
+            get { return (bool)GetValue(IsNormalizedProperty); }
             set { SetValue(IsNormalizedProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for QuickAccessCommands.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IsNormalizedProperty =
-            DependencyProperty.Register("IsNormalized", typeof (bool), typeof (MetroWindow),
+            DependencyProperty.Register("IsNormalized", typeof(bool), typeof(MetroWindow),
                 new PropertyMetadata(false));
 
         public bool IsMaximized {
-            get { return (bool) GetValue(IsMaximizedProperty); }
+            get { return (bool)GetValue(IsMaximizedProperty); }
             set { SetValue(IsMaximizedProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for IsMaximized.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IsMaximizedProperty =
-            DependencyProperty.Register("IsMaximized", typeof (bool), typeof (MetroWindow), new PropertyMetadata(false));
+            DependencyProperty.Register("IsMaximized", typeof(bool), typeof(MetroWindow), new PropertyMetadata(false));
 
         public Brush AccentBrush {
-            get { return (Brush) GetValue(AccentBrushProperty); }
+            get { return (Brush)GetValue(AccentBrushProperty); }
             set { SetValue(AccentBrushProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for AccentBrush.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty AccentBrushProperty =
-            DependencyProperty.Register("AccentBrush", typeof (Brush), typeof (MetroWindow), new PropertyMetadata(null));
+            DependencyProperty.Register("AccentBrush", typeof(Brush), typeof(MetroWindow), new PropertyMetadata(null));
 
         public Brush HoverBrush {
-            get { return (Brush) GetValue(HoverBrushProperty); }
+            get { return (Brush)GetValue(HoverBrushProperty); }
             set { SetValue(HoverBrushProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for HoverBrush.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty HoverBrushProperty =
-            DependencyProperty.Register("HoverBrush", typeof (Brush), typeof (MetroWindow), new PropertyMetadata(null));
+            DependencyProperty.Register("HoverBrush", typeof(Brush), typeof(MetroWindow), new PropertyMetadata(null));
 
         public string SearchText {
-            get { return (string) GetValue(SearchTextProperty); }
+            get { return (string)GetValue(SearchTextProperty); }
             set { SetValue(SearchTextProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for SearchText.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty SearchTextProperty =
-            DependencyProperty.Register("SearchText", typeof (string), typeof (MetroWindow),
+            DependencyProperty.Register("SearchText", typeof(string), typeof(MetroWindow),
                 new PropertyMetadata(string.Empty));
 
         #endregion
@@ -191,36 +191,72 @@ namespace Crystalbyte.Paranoia.UI {
         // ReSharper disable MemberCanBePrivate.Local
 
         private const int MONITOR_DEFAULTTONEAREST = 0x00000002;
-        private const int WINDOWPOSCHANGING = 0x0046;
+        private const int WM_WINDOWPOSCHANGING = 0x0046;
         private const int WM_GETMINMAXINFO = 0x0024;
+        private const int SWP_NOMOVE = 0x0002;
 
-        private static IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled) {
+        private IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled) {
             switch (msg) {
                 case WM_GETMINMAXINFO:
                     WmGetMinMaxInfo(hwnd, lParam);
                     handled = true;
                     break;
+                case WM_WINDOWPOSCHANGING:
+                    handled = WmPositionChanging(lParam);
+                    break;
+
             }
 
             return IntPtr.Zero;
         }
 
+        private bool WmPositionChanging(IntPtr lParam) {
+            var target = _hwndSource.CompositionTarget;
+            if (target == null) {
+                return false;
+            }
+
+            var matrix = target.TransformToDevice;
+            var minWidth = Convert.ToInt32(matrix.Transform(new Point(MinWidth, 0)).X);
+            var minHeight = Convert.ToInt32(matrix.Transform(new Point(0, MinHeight)).Y);
+
+            var pos = (WINDOWPOS)Marshal.PtrToStructure(lParam, typeof(WINDOWPOS));
+            if ((pos.flags & SWP_NOMOVE) != 0) {
+                return false;
+            }
+
+            var isAdjusted = false;
+            if (pos.cx < minWidth) {
+                pos.cx = minWidth; isAdjusted = true;
+            }
+            if (pos.cy < minHeight) {
+                pos.cy = minHeight; isAdjusted = true;
+            }
+
+            if (!isAdjusted) {
+                return false;
+            }
+
+            Marshal.StructureToPtr(pos, lParam, true);
+            return true;
+        }
+
         private static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam) {
-            var mmi = (MINMAXINFO) Marshal.PtrToStructure(lParam, typeof (MINMAXINFO));
+            var mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
 
             // Adjust the maximized size and position to fit the work area of the correct monitor
 
             var monitor = NativeMethods.MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
 
             if (monitor != IntPtr.Zero) {
-                var monitorInfo = new MONITORINFOEX
-                {
-                    cbSize = Marshal.SizeOf(typeof (MONITORINFOEX))
+                var monitorInfo = new MONITORINFOEX {
+                    cbSize = Marshal.SizeOf(typeof(MONITORINFOEX))
                 };
 
                 NativeMethods.GetMonitorInfo(monitor, ref monitorInfo);
                 var rcWorkArea = monitorInfo.rcWork;
                 var rcMonitorArea = monitorInfo.rcMonitor;
+
                 mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
                 mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
                 mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
@@ -240,7 +276,18 @@ namespace Crystalbyte.Paranoia.UI {
         }
 
         [StructLayout(LayoutKind.Sequential)]
-        public struct POINT {
+        private struct WINDOWPOS {
+            public IntPtr hwnd;
+            public IntPtr hwndInsertAfter;
+            public int x;
+            public int y;
+            public int cx;
+            public int cy;
+            public int flags;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT {
             public int x;
             public int y;
 
@@ -251,7 +298,7 @@ namespace Crystalbyte.Paranoia.UI {
         }
 
         [StructLayout(LayoutKind.Sequential)]
-        public struct MINMAXINFO {
+        private struct MINMAXINFO {
             public POINT ptReserved;
             public POINT ptMaxSize;
             public POINT ptMaxPosition;
@@ -260,7 +307,7 @@ namespace Crystalbyte.Paranoia.UI {
         };
 
         [StructLayout(LayoutKind.Sequential)]
-        public struct RECT {
+        private struct RECT {
             public int left;
             public int top;
             public int right;
@@ -268,12 +315,13 @@ namespace Crystalbyte.Paranoia.UI {
         }
 
         [StructLayout(LayoutKind.Sequential)]
-        public struct MONITORINFOEX {
+        private struct MONITORINFOEX {
             public int cbSize;
             public RECT rcMonitor; // Total area
             public RECT rcWork; // Working area
             public int dwFlags;
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x20)] public char[] szDevice;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x20)]
+            public char[] szDevice;
         }
 
         // ReSharper restore InconsistentNaming
