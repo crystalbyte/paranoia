0fc171d1ec87619b4034ec28349ce8ed2280557e
Marvin <MSchluch@hotmail.de>
2014-08-25 20:50:37 +0200

    step one for attachments

--- src/Crystalbyte.Paranoia/ResourceInterceptor.cs
+++ src/Crystalbyte.Paranoia/ResourceInterceptor.cs
@@ -7,32 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Crystalbyte.Paranoia
-{
-    public class ResourceInterceptor : IResourceInterceptor
-    {
-        public bool OnFilterNavigation(NavigationRequest request)
-        {
+namespace Crystalbyte.Paranoia {
+    public class ResourceInterceptor : IResourceInterceptor {
+        public bool OnFilterNavigation(NavigationRequest request) {
             return false;
         }
 
-        public ResourceResponse OnRequest(ResourceRequest request)
-        {
+        public ResourceResponse OnRequest(ResourceRequest request) {
             if (request.Url.Scheme != "asset")
                 return null;
 
             Debug.WriteLine(request.Url.AbsolutePath);
 
-            //TODO add more resources
+            //TODO add more resources if needed
             if (request.Url.AbsolutePath.EndsWith(".js")
                 || request.Url.AbsolutePath.EndsWith(".css")
-                || request.Url.AbsolutePath.EndsWith(".png"))
-            {
+                || request.Url.AbsolutePath.EndsWith(".png")) {
                 var file = Environment.CurrentDirectory + request.Url.AbsolutePath.Replace("/message", string.Empty);
                 if (File.Exists(file))
                     return ResourceResponse.Create(file);
-                else
-                {
+                else {
                     throw new Exception("resource could not be resolved");
                 }
             }
---
+++ src/Crystalbyte.Paranoia/UI/Commands/AddAttachmentCommand.cs
@@ -0,0 +1,34 @@
+﻿using Crystalbyte.Paranoia.Contexts;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Crystalbyte.Paranoia.UI.Commands {
+    internal class AddAttachmentCommand : ICommand {
+
+        private readonly MailCompositionContext _context;
+        public AddAttachmentCommand(MailCompositionContext context) {
+            _context = context;
+        }
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter) {
+            var dialoge = new OpenFileDialog();
+
+            Nullable<bool> result = dialoge.ShowDialog();
+
+            if (result != null && result == true) {
+                string filename = dialoge.FileName;
+                _context.Attachments.Add(new AttachmentContext(filename));
+            }
+        }
+    }
+}
