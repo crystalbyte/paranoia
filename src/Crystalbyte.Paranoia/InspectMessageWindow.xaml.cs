ac03287470d04303af855dc129280ec520a5cb9d
Marvin <MSchluch@hotmail.de>
2014-10-14 23:26:53 +0200

    inspector window implemented not done jet

---
+++ src/Crystalbyte.Paranoia/InspectMessageWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Crystalbyte.Paranoia {
+    /// <summary>
+    /// Interaktionslogik für InspectMessageWindow.xaml
+    /// </summary>
+    public partial class InspectMessageWindow {
+        public InspectMessageWindow() {
+            InitializeComponent();
+        }
+
+        public InspectMessageWindow(string file)
+            : this() {
+        }
+
+        public InspectMessageWindow(MailMessageContext message)
+            : this() {
+                var source = App.Context.Source;
+                blub.Source = source; 
+        }
+    }
+}
