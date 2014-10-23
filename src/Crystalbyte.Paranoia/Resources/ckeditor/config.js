20d85b0a7b44d783c444fea7a406287da5edc59c
crystalbyte <alexander.wieser@crystalbyte.de>
2014-10-23 22:07:49 +0200

    Massive changes to the ckeditor style.

--- src/Crystalbyte.Paranoia/Resources/ckeditor/config.js
+++ src/Crystalbyte.Paranoia/Resources/ckeditor/config.js
@@ -1,15 +1,40 @@
 /**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function (config) {
-    //config.width = '99,9%';
-    //config.height = '100%';
-    config.disableNativeSpellChecker = false;
-    config.resize_enabled = false;
-    //config.uiColor = 'red';
-    //config.skin = 'office2013';
-    config.skin = 'moono-dark';
-    config.removePlugins = 'elementspath';
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		//{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+	    { name: 'undo' },
+		//{ name: 'editing', groups: ['find', 'selection', 'spellchecker'] },
+        { name: 'styles' },
+        { name: 'others' },
+        { name: 'colors' },
+        { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'about' }
+	];
+
+	config.removePlugins = 'elementspath';
+
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	//config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
 };
