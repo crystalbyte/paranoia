d6023b9ac951aa71e7e393720954cf51ad6538d4
crystalbyte <alexander.wieser@crystalbyte.de>
2014-10-27 15:42:32 +0100

    Refactored block notice bar into UserControl. Both, the InspectionWindow and MessagePage now have blocking notices.

---
+++ src/Crystalbyte.Paranoia/UI/BlockNoticeControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Crystalbyte.Paranoia.UI {
+    /// <summary>
+    /// Interaction logic for BlockNoticeControl.xaml
+    /// </summary>
+    public partial class BlockNoticeControl {
+        public BlockNoticeControl() {
+            InitializeComponent();
+            DataContext = App.Context.SelectedMessage;
+            App.Context.MessageSelectionChanged 
+                += (sender, e) => DataContext = App.Context.SelectedMessage;
+        }
+    }
+}
