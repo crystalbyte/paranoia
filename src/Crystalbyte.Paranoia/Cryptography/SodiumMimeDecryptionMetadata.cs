8e7fc04f9b1f65d32f1608a45d8014ed2611316b
crystalbyte <alexander.wieser@crystalbyte.de>
2015-11-03 22:38:55 +0100

    Missed metadata.

---
+++ src/Crystalbyte.Paranoia/Cryptography/SodiumMimeDecryptionMetadata.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crystalbyte.Paranoia {
+
+    internal sealed class SodiumMimeDecryptionMetadata {
+
+        #region Private Fields
+
+        private List<EncryptedSecret> _entries;
+
+        #endregion
+
+        #region Construction
+
+        public SodiumMimeDecryptionMetadata() {
+            _entries = new List<EncryptedSecret>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsEncrypted { get; set; }
+
+        public string Version { get; set; }
+
+        public byte[] Secret { get; set; }
+
+        public byte[] EncryptedMessage { get; set; }
+
+        public byte[] Nonce { get; set; }
+
+        public List<EncryptedSecret> Entries {
+            get { return _entries; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public string ToMimeHeader() {
+            return string.Format("v={0}; n={1}", Version, Nonce);
+        }
+
+        #endregion
+    }
+}
